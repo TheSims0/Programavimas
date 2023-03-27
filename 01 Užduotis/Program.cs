e8ab8c8e9d4e67f9793bf62166a1c961273cec2f
TheSims0 <mahtynas@gmail.com>
2023-03-27 23:40:19 +0300

    ND7

---
+++ 01 Užduotis/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Užduotis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Labas, aš Simonas");
+            int amzius = 24;
+            Console.WriteLine($"Įvestas amžius: {amzius}");
+            Console.WriteLine();
+            Console.WriteLine("{0}{0}{0}{0}{0}", amzius);
+            Console.WriteLine("{0} {0} {0} {0} {0}", amzius);
+        }
+    }
+}
---
+++ 02 Užduotis/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Užduotis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pats sugalvojau taip
+            Console.WriteLine("----------------------------------- \n" +
+                              "|                                 |\n" +
+                              "|                                 |\n" +
+                              "|           Mano vardas           |\n" +
+                              "|             Simonas             |\n" +
+                              "|                                 |\n" +
+                              "|                                 |\n" +
+                              "-----------------------------------");
+            //O čia su ChatGPT pagalba pavyko rezultata pagerint taip kad lentelės kraštai neišsilygiuotu ir tekstas visada būtu centruotas
+            string name1 = "Mano vardas";
+            string name2 = "Simonas";
+            int totalWidth = 50;
+            int nameWidth = name1.Length;
+
+            Console.WriteLine(new string('-', totalWidth));
+            Console.WriteLine($"|{new string(' ', totalWidth - 2)}|");
+            Console.WriteLine($"|{new string(' ', (totalWidth - nameWidth - 2) / 2)}{name1}{new string(' ', (totalWidth - nameWidth - 1) / 2)}|");
+            Console.WriteLine($"|{new string(' ', (totalWidth - name2.Length - 2) / 2)}{name2}{new string(' ', (totalWidth - name2.Length - 1) / 2)}|");
+            Console.WriteLine($"|{new string(' ', totalWidth - 2)}|");
+            Console.WriteLine(new string('-', totalWidth));
+
+
+        }
+    }
+}
