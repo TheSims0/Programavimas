4bfb9237b6102488fb49467d0fdec7023c71b370
TheSims0 <mahtynas@gmail.com>
2023-04-03 13:13:03 +0300

    ND8

--- Uzduotis09/Program.cs
+++ Uzduotis09/Program.cs
@@ -5,7 +5,47 @@ namespace Uzduotis09
     {
         static void Main(string[] args)
         {
-
+            Console.Write("Iveskite savo amziu: ");
+            int amzius = Convert.ToInt16(Console.ReadLine());
+            if (amzius >= 18)
+            {
+                Console.WriteLine("Jus galite balsuoti!");
+            }
+            Console.WriteLine("Iveskite pirma skaiciu");
+            double pirmas = Convert.ToDouble(Console.ReadLine());
+            if (pirmas < 0)
+            {
+                Console.WriteLine("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            }
+            else if (pirmas > 0)
+            {
+                Console.WriteLine("Puikiai atlikta uzduotis");
+            }
+            if (pirmas % 2 == 0)
+            {
+                Console.WriteLine("Puiku, skaicius lyginis!");
+            }
+            if (pirmas % 4 == 0)
+            {
+                Console.WriteLine("Puiku, skaicius dalinais is 4!");
+            }
+            if (pirmas > 10)
+            {
+                Console.WriteLine("Puiku, skaicius didesnis uz 10!");
+            }
+            Console.Write("Iveskite pirma pazymi: ");
+            int matematika = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Iveskite antra pazymi: ");
+            double lietuviu = Convert.ToDouble(Console.ReadLine());
+            Console.Write($"Vidurkis: {(matematika + lietuviu) / 2} ");
+            if ((matematika + lietuviu) / 2 >= 5)
+            {
+                Console.WriteLine("VALIO!");
+            }
+            else
+                    {
+                Console.WriteLine(":(");
+            }
         }
     }
 }
---
+++ Uzduotis10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Uzduotis10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite skaiciu");
+            int skaicius = Convert.ToInt32(Console.ReadLine());
+            if (skaicius % 2 == 0)
+            {
+                Console.WriteLine($"Jusu skaicius {skaicius} ir jis pakeltas 5 laipsniu {Math.Pow(skaicius,5)}");
+            }
+            if (skaicius % 4 != 0)
+            {
+                Console.WriteLine("Iveskite antra skaiciu");
+                int antras = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Skaiciu sandauga {skaicius * antras}, o suma {skaicius + antras}");
+            }
+        }
+    }
+}
