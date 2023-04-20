0c5e92dd58a886a2caf35277200510600a6522db
TheSims0 <mahtynas@gmail.com>
2023-04-20 18:15:11 +0300

    ND10 UZD29

---
+++ Uzduotis29/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Uzduotis29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Iveskite savo varda: ");
+            string vardas = Console.ReadLine();
+            Console.Write("Iveskite savo amziu: ");
+            int amzius = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Iveskite savo megstamiausia hobi: ");
+            string hobis = Console.ReadLine();
+            Asmenybe(vardas, amzius,hobis);
+        }
+        private static void Asmenybe(string vardas, int amzius, string hobis)
+        {
+            Console.WriteLine($"Labas, {vardas} ({amzius}).");
+            Console.WriteLine($"Tavo hobis: {hobis}");
+        }
+    }
+}
