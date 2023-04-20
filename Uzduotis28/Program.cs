459a5b8801430558464a74187b990edbdd2df7d2
TheSims0 <mahtynas@gmail.com>
2023-04-20 18:14:53 +0300

    ND10 UZD28

---
+++ Uzduotis28/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Uzduotis28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodas();
+        }
+        private static void Metodas()
+        {
+            Console.WriteLine("Prpograma pradejo darba!");
+        }
+    }
+}
