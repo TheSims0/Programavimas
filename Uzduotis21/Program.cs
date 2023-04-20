054029afe1bbce2f3d0c5edd958c11de889fdb47
TheSims0 <mahtynas@gmail.com>
2023-04-20 15:41:55 +0300

    ND10 UZD21

---
+++ Uzduotis21/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+namespace Uzduotis21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] pazymiai = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            string[] automobiliai = new string[4];
+            automobiliai[0] = "Audi";
+            automobiliai[1] = "BMW";
+            automobiliai[2] = "Subaru";
+            automobiliai[3] = "Toyota";
+            Console.WriteLine(pazymiai.Length);
+            Console.WriteLine(automobiliai.Length);
+            Console.WriteLine();
+            Console.WriteLine(pazymiai.First());
+            Console.WriteLine(automobiliai.First());
+            Console.WriteLine();
+            Console.WriteLine(pazymiai.Last());
+            Console.WriteLine(automobiliai.Last());
+            Console.WriteLine();
+            Console.WriteLine(pazymiai[pazymiai.Length - 1]);
+            Console.WriteLine(automobiliai[automobiliai.Length - 1]);
+        }
+    }
+}
