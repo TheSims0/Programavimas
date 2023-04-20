b15f9a1a23c2d0e0407103016639360b748920a3
TheSims0 <mahtynas@gmail.com>
2023-04-20 18:15:36 +0300

    ND10 UZD31

---
+++ Uzduotis31/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Uzduotis31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program programa = new Program();
+            int skaicius1 = 10;
+            int skaicius2 = 20;
+            int skaicius3 = 30;
+            Console.WriteLine(programa.Suma(skaicius1, skaicius2, skaicius3));
+            Console.WriteLine(programa.Dalyba(skaicius1, skaicius2));
+        }
+        private int Suma(int skaicius1, int skaicius2, int skaicius3)
+        {
+            return skaicius1 + skaicius2 + skaicius3;
+        }
+        private double Dalyba(int skaicius1, int skaicius2)
+        {
+            return Convert.ToDouble(skaicius1) / Convert.ToDouble(skaicius2);
+        }
+    }
+}
