611c390e988c85242294aa177533c3e6789428b3
TheSims0 <mahtynas@gmail.com>
2023-04-20 18:16:02 +0300

    ND10 UZD32

---
+++ Uzduotis32/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Uzduotis32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            Console.Write("Iveskite savo varda: ");
+            string vardas = Console.ReadLine();
+            Console.WriteLine(prog.Pasisveikinimas(vardas));
+        }
+        private string Pasisveikinimas(string vardas)
+        {
+            return $"Sveiki, {vardas}!";
+        }
+    }
+}
